13fa3ce7cb050efc64eac63e7c270c9dba05a10b
Griffin Fore <griffinfore@gmail.com>
2023-10-23 16:16:15 -0700

    Redo of First Print

---
+++ First_Java.java
@@ -0,0 +1,7 @@
+public class First_Java {
+    public static void main(String[] args) {
+        System.out.println("My name is Griffin Fore");
+        System.out.println("I am 27 winters old");
+        System.out.println("I hail from the land down under");
+    }
+}
